1449862fd926107ead85bc9755adf1c1dfcfa150
Alex <chl203@student.bham.ac.uk>
2024-02-29 12:48:21 +0000

    feat: Create inital files for map display feature

---
+++ src/main/webapp/app/feature/map-display/map-display.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-map-display',
+  templateUrl: './map-display.component.html',
+  styleUrls: ['./map-display.component.scss'],
+})
+export class MapDisplayComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
