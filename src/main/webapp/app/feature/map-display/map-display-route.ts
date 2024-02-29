b3164095b5ae2887cf4b64ba24c7f205c5c03669
Alex <chl203@student.bham.ac.uk>
2024-02-29 14:23:41 +0000

    feat: Changed navbar title and added map icon that leads to map display page

---
+++ src/main/webapp/app/feature/map-display/map-display-route.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { MapDisplayComponent } from './map-display.component';
+
+export const MAP_DISPLAY_ROUTE: Route = {
+  path: '',
+  component: MapDisplayComponent,
+};
