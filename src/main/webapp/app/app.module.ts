117685cae39620e9c7e31921f7c03a2013b7afdf
Alex <chl203@student.bham.ac.uk>
2024-03-02 17:03:01 +0000

    refactor: move map-display component to /app

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -23,7 +23,7 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
-import { MapDisplayComponent } from './feature/map-display/map-display.component';
+import { MapDisplayComponent } from './map-display/map-display.component';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
--- src/main/webapp/app/map-display/map-display.module.ts
+++ src/main/webapp/app/map-display/map-display.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { MAP_DISPLAY_ROUTE } from './map-display-route';
 import { MapDisplayComponent } from './map-display.component';
 import { RouterModule } from '@angular/router';
