98423413033070e02cdacf21f47ecb959a58a9d0
Alex <chl203@student.bham.ac.uk>
2024-03-02 21:10:00 +0000

    refactor: remove duplicate map display files

--- src/main/webapp/app/feature/map-display/map-display-route.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import { MapDisplayComponent } from './map-display.component';
-
-export const MAP_DISPLAY_ROUTE: Route = {
-  path: '',
-  component: MapDisplayComponent,
-};
