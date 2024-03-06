0245231b31880ec8b10ffe095e69156dc7ca4469
Tonny Kilonzi <tmk125@student.bham.ac.uk>
2024-03-06 04:32:13 +0000

    Changed navbar colours and font colours, added a carousel slider for the homepage

---
+++ src/main/webapp/app/carousel/carousel.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
+})
+export class CarouselComponent implements OnInit {
+  images = [
+    { src: '../content/images/ITALY.jpg', alt: 'Image 1' },
+    { src: '../content/images/VIETNAM.jpg', alt: 'Image 2' },
+    { src: '../content/images/PARIS.jpg', alt: 'Image 3' },
+    { src: '../content/images/SWITZERLAND.jpg', alt: 'Image 4' },
+    { src: '../content/images/INDIA.jpg', alt: 'Image 5' },
+
+    // Add more images as needed
+  ];
+  currentIndex = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.showSlide(this.currentIndex);
+  }
+
+  showSlide(index: number): void {
+    this.currentIndex = index;
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.showSlide(this.currentIndex);
+  }
+
+  prevSlide(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.showSlide(this.currentIndex);
+  }
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,8 +5,10 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
+import { CarouselComponent } from '../carousel/carousel.component';
+
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CarouselComponent],
 })
 export class HomeModule {}
