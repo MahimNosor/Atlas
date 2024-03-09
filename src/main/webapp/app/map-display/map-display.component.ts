65283c15fbf6b0ccecf559f9903457b13934b050
Alex <chl203@student.bham.ac.uk>
2024-03-09 21:07:08 +0000

    fix: fix map markers not appearing

--- src/main/webapp/app/map-display/map-display.component.ts
+++ src/main/webapp/app/map-display/map-display.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { icon, Marker } from 'leaflet';
 import * as L from 'leaflet';
 import '../../../../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js';
 import '../../../../../node_modules/leaflet-control-geocoder/dist/Control.Geocoder.js';
+
 @Component({
   selector: 'jhi-map-display',
   templateUrl: './map-display.component.html',
@@ -11,12 +13,41 @@ export class MapDisplayComponent implements OnInit {
   travelTime!: string;
   travelDistance!: string;
   summaryLoaded = false;
+
   private map: any;
 
   constructor() {}
 
   ngOnInit(): void {
     this.initMap();
+  }
+
+  private initMarker(): void {
+    const iconUrl = '../../content/images/marker-icon.png';
+    const shadowUrl = '../../content/images/marker-shadow.png';
+    const iconDefault = icon({
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41],
+    });
+    Marker.prototype.options.icon = iconDefault;
+  }
+
+  private initRouting(): void {
+    const control = L.Routing.control({
+      geocoder: L.Control.Geocoder.nominatim(),
+      routeWhileDragging: true,
+    }).addTo(this.map);
+
+    control.on('routesfound', (e: any) => {
+      this.travelTime = (e.routes[0].summary.totalTime / 3600).toFixed(2);
+      this.travelDistance = (e.routes[0].summary.totalDistance / 1000).toFixed(2);
+      this.summaryLoaded = true;
+    });
   }
 
   private initMap(): void {
@@ -31,15 +62,8 @@ export class MapDisplayComponent implements OnInit {
 
     L.Control.geocoder().addTo(this.map);
 
-    const control = L.Routing.control({
-      geocoder: L.Control.Geocoder.nominatim(),
-      routeWhileDragging: true,
-    }).addTo(this.map);
+    this.initMarker();
 
-    control.on('routesfound', (e: any) => {
-      this.travelTime = (e.routes[0].summary.totalTime / 3600).toFixed(2);
-      this.travelDistance = (e.routes[0].summary.totalDistance / 1000).toFixed(2);
-      this.summaryLoaded = true;
-    });
+    this.initRouting();
   }
 }
