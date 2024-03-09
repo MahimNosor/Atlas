5ed30d5c626a857fbb126817bca4a3c54d7a54e3
MahimNosor <mahimn109@gmail.com>
2024-03-09 14:12:09 +0000

    Page images show up properly along with the caption

--- src/main/webapp/app/user-routes/user-routes.component.ts
+++ src/main/webapp/app/user-routes/user-routes.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-routes.component.scss'],
 })
 export class UserRoutesComponent implements OnInit {
+  images: { src: string; caption: string }[] = [
+    { src: 'content/images/london.png', caption: 'London<br />30/01/2024' },
+    { src: 'content/images/paris.jpg', caption: 'Paris<br />29/01/2024' },
+    { src: 'content/images/dhaka.png', caption: 'Dhaka<br />28/01/2024' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
