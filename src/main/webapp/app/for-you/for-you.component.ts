7b8f07de7b5a03fa19841569dbaaa7f00f889d34
cwc172 <cwc172@students@bham.ac.uk>
2024-03-20 12:51:37 +0800

    for you page update:linking ts file with html file

--- src/main/webapp/app/for-you/for-you.component.ts
+++ src/main/webapp/app/for-you/for-you.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { TagService } from 'app/entities/tag/service/tag.service';
+import { UserService } from 'app/entities/user/user.service';
+//import { RouteService } from './service/route.service';
 
 @Component({
   selector: 'jhi-for-you',
   templateUrl: './for-you.component.html',
   styleUrls: ['./for-you.component.scss'],
 })
-export class ForYouComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ForYouComponent {
+  // Define routes array
+  routes = [
+    { routeID: '03172', tags: ['Tag1', 'Tag2'], distance: 6, rating: 4.9 },
+    { routeID: '21172', tags: ['Tag3', 'Tag4'], distance: 5, rating: 4.8 },
+    { routeID: '03142', tags: ['Tag5', 'Tag6'], distance: 2, rating: 4.4 },
+  ];
 }
